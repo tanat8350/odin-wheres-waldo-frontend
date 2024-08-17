4a9461796d771690cbc591d0b8b1d15a084476e1
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-08-18 02:47:38 +0700

    finish add found marks

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,32 +1,43 @@
 import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 const MAX_DIFF_X = 50;
 const MAX_DIFF_Y = 100;
+const MARK_SIZE = 50;
+const STICKY_SIZE = 100;
 
 const Game = () => {
   const [data, setData] = useState(null);
-  // const [circle, setCircle]
+  const [circles, setCircles] = useState([]);
   const locations = useRef(null);
+  const result = useRef(null);
 
   const onClick = (e) => {
-    const t = e.target.getBoundingClientRect();
-    const xx = e.clientX - t.left;
-    const yy = e.clientY - t.left;
-    console.log(t, xx, yy);
+    const px = e.pageX;
+    const py = e.pageY;
+    console.log(px, py);
     const copy = [...locations.current];
     let found = null;
+    let originalIndex = null;
     for (const [index, value] of locations.current.entries()) {
-      const [x, y] = value.split(',');
-      if (
-        Math.abs(x - e.pageX) < MAX_DIFF_X &&
-        Math.abs(y - e.pageY) < MAX_DIFF_Y
-      ) {
+      const [i, x, y] = value.split(',');
+      if (Math.abs(x - px) < MAX_DIFF_X && Math.abs(y - py) < MAX_DIFF_Y) {
         found = index;
+        originalIndex = i;
         break;
       }
     }
-    if (found === 0 || found) copy.splice(found, 1);
-    locations.current = copy;
+    if (found === 0 || found) {
+      result.current[originalIndex] = `${px},${py}`;
+      copy.splice(found, 1);
+      locations.current = copy;
+      console.log('result', result.current);
+      setCircles([
+        ...circles,
+        { key: found, x: px - MARK_SIZE / 2, y: py - MARK_SIZE / 2 },
+      ]);
+    }
     if (locations.current.length === 0) {
       console.log('you won');
     }
@@ -38,8 +49,10 @@ const Game = () => {
       })
       .then((json) => {
         setData(json);
-        locations.current = json.locations;
-        console.log(json);
+        locations.current = json.locations.map((value, index) => {
+          return `${index},${value}`;
+        });
+        result.current = new Array(json.locations.length);
       });
   }, []);
   return (
@@ -50,28 +63,43 @@ const Game = () => {
           <h2>Image Name: {data.name}</h2>
           <div>
             <div className="sticky">
-              <img
-                src={'http://localhost:3000/images/' + data.id + '/1.jpg'}
-                alt="1"
-              />
-              <img
-                src={'http://localhost:3000/images/' + data.id + '/2.jpg'}
-                alt="2"
-              />
-              <img
-                src={'http://localhost:3000/images/' + data.id + '/3.jpg'}
-                alt="3"
-              />
+              {data &&
+                data.locations.map((_, i) => {
+                  return (
+                    <img
+                      src={`http://localhost:3000/images/${data.id}/${i + 1}.jpg`}
+                      alt={i}
+                      className={result.current[i] && 'found'}
+                      style={{ height: `${STICKY_SIZE}px` }}
+                    />
+                  );
+                })}
             </div>
             <img
               src={'http://localhost:3000/images/' + data.id + '/game.jpg'}
               alt="game"
               onClick={onClick}
             />
+            {circles.length > 0 &&
+              circles.map((c) => {
+                return (
+                  <FontAwesomeIcon
+                    icon={faCircleCheck}
+                    bounce
+                    className="fail-mark"
+                    style={{
+                      position: 'absolute',
+                      top: `${c.y}`,
+                      left: `${c.x}`,
+                      color: '#00ff00',
+                      height: `${MARK_SIZE}px`,
+                    }}
+                  />
+                );
+              })}
           </div>
         </>
       )}
-      <button onClick={() => console.log(data)}>click</button>
     </>
   );
 };
